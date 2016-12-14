dc9303aa4ad73be7f852cf4178dcc27b4600688b
Stefan Lapers <stefan@lapers.be>
2016-12-14 19:24:58 +0100

    day7 - part2

--- lib/advent_of_code/day7.ex
+++ lib/advent_of_code/day7.ex
@@ -4,17 +4,22 @@ defmodule AdventOfCode.Day7 do
 
   def part1() do
     Common.read_file(7)
-    |> Enum.filter(&supports_tls?/1)
-    |> Enum.count()
+      |> Enum.filter(&supports_tls?/1)
+      |> Enum.count()
   end
 
   def supports_tls?(line) do
+    {nets, hypernets} = parse_address(line)
+    (not Enum.any?(hypernets, &has_abba?/1)) and Enum.any?(nets, &has_abba?/1)
+  end
+
+  def parse_address(line) do
     nets = Regex.split(~r/\[[a-z]+\]/, line)
     hypernets =
       Regex.scan(~r/\[([a-z]+)\]/, line)
-      |> Enum.map(fn l -> List.last(l) end)
+        |> Enum.map(fn l -> List.last(l) end)
 
-    (not Enum.any?(hypernets, &has_abba?/1)) and Enum.any?(nets, &has_abba?/1)
+    {nets, hypernets}
   end
 
   def has_abba?(string) when is_binary(string), do: has_abba?(String.graphemes(string))
@@ -23,6 +28,51 @@ defmodule AdventOfCode.Day7 do
       [a,b,b,a] when a != b -> true
       [a,b,b,a | _] when a != b -> true
       [_,x,y,z | tail] -> has_abba?([x,y,z] ++ tail)
+      _ -> false
+    end
+  end
+
+  # Part 2 starts here
+  # --
+  def part2() do
+    Common.read_file(7)
+      |> Enum.filter(&supports_ssl?/1)
+      |> Enum.count
+  end
+
+  def supports_ssl?(line) do
+    {nets, hypernets} = parse_address(line)
+    babs = get_bab_patterns(hypernets)
+    Enum.any?(nets, &has_matching_aba_for?(&1,babs))
+  end
+
+  def get_bab_patterns(nets, acc \\ []) when is_list(nets) do
+    case nets do
+      [net | rest] -> get_bab_patterns(rest, get_bab_patterns_in(String.graphemes(net)) ++ acc)
+      [] -> acc
+    end
+  end
+
+  defp get_bab_patterns_in(graphemes, acc \\ []) do
+    case graphemes do
+      [b,a,b] when a != b -> ([[b,a,b]] ++ acc) |> Enum.sort |> Enum.dedup
+      [b,a,b | tail] when a != b -> get_bab_patterns_in([a,b] ++ tail, [[b,a,b]] ++ acc)
+      [_,x,y | tail] -> get_bab_patterns_in([x,y] ++ tail, acc)
+      _ -> acc
+    end
+  end
+
+  def has_matching_aba_for?(string, babs) when is_binary(string) do
+    graphemes = String.graphemes(string)
+    babs
+      |> Enum.any?(&has_matching_aba_in?(&1,graphemes))
+  end
+
+  defp has_matching_aba_in?([b,a,b], graphemes) do
+    case graphemes do
+      [x,y,z] when [x,y,z] == [a,b,a] -> true
+      [x,y,z | _] when [x,y,z] == [a,b,a] -> true
+      [_,y,z | tail] -> has_matching_aba_in?([b,a,b], [y,z] ++ tail)
       _ -> false
     end
   end
--- test/advent_of_code/day7_test.exs
+++ test/advent_of_code/day7_test.exs
@@ -1,5 +1,5 @@
 defmodule AdventOfCodeTest.Day7 do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   alias AdventOfCode.Day7, as: D7
 
 
@@ -22,5 +22,22 @@ defmodule AdventOfCodeTest.Day7 do
 
   test "part 1 returns the correct answer" do
     assert 105 == D7.part1()
+  end
+
+  # Part 2 stuff
+
+  test "get_abas should return a non duplicate list of aba patterns" do
+    assert [["a","b", "a"],["b","a","b"]] == D7.get_bab_patterns(["ababa"])
+  end
+
+  test "supports_ssl? returns true when ssl aba and bab are present" do
+    assert true == D7.supports_ssl?("aba[bab]xyz")
+    assert false == D7.supports_ssl?("xyx[xyx]xyx")
+    assert true == D7.supports_ssl?("zazbz[bzb]cdb")
+    assert true == D7.supports_ssl?("aaa[kak]eke[kek]")
+  end
+
+  test "part 2 returns the correct answer" do
+    assert 258 == D7.part2()
   end
 end
