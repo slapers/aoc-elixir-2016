c3b6b8d40d84f6d883061451b34c51e7053b5ca6
Stefan Lapers <stefan@lapers.be>
2016-12-14 12:51:40 +0100

    day 6 - part 1

---
+++ lib/advent_of_code/day6.ex
@@ -0,0 +1,30 @@
+defmodule AdventOfCode.Day6 do
+
+  alias AdventOfCode.Common, as: Common
+
+  def part1() do
+    Common.read_file(6)
+    |> Enum.map(&String.graphemes/1)
+    |> List.zip
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.map(&reduce_highest_occurrence/1)
+    |> Enum.join()
+  end
+
+  def reduce_highest_occurrence(list) do
+    Enum.reduce(list, %{}, &update_occurence/2)
+    |> Map.to_list()
+    |> Enum.sort(&sort_by_count/2)
+    |> List.first
+    |> elem(0)
+  end
+
+  def update_occurence(el, map) do
+    case Map.fetch(map, el) do
+      :error -> Map.put(map, el, 1)
+      {:ok, num} -> Map.put(map, el, num + 1)
+    end
+  end
+
+  def sort_by_count({_x,xc}, {_y,yc}), do: xc > yc
+end
---
+++ test/advent_of_code/day6_test.exs
@@ -0,0 +1,8 @@
+defmodule AdventOfCodeTest.Day6 do
+  use ExUnit.Case
+  alias AdventOfCode.Day6, as: D6
+
+  test "part1 gives the correct answer" do
+    D6.part1() |> IO.inspect()
+  end
+end
