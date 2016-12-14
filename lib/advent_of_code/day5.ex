67e9c40a2cb57687cb6203cc20b4cf3f527f43f3
Stefan Lapers <stefan@lapers.be>
2016-12-14 12:20:44 +0100

    day 5 -p 2

--- lib/advent_of_code/day5.ex
+++ lib/advent_of_code/day5.ex
@@ -1,23 +1,55 @@
 defmodule AdventOfCode.Day5 do
 
   def hash(input), do: hash(input, 1)
-  def hash(input, counter) do
+  defp hash(input, counter) do
     res = :crypto.hash(:md5, "#{input}#{counter}")
     |> Base.encode16(case: :lower)
 
     case String.split_at(res, 5) do
-      {"00000", remainder} -> {String.at(remainder, 0), counter}
+      {"00000", remainder} -> {remainder, counter}
       _ -> hash(input, counter + 1)
     end
   end
 
   def create_password(input), do: create_password(input, "", 1)
   def create_password(input, password, counter) do
-    {letter, new_counter} = hash(input, counter)
+    {letters, new_counter} = hash(input, counter)
+    letter = String.at(letters,0)
     case String.length(password) do
       size when size >= 7 -> password <> letter
       size when size < 7 -> create_password(input, password <> letter, new_counter + 1)
     end
   end
 
+  def create_password_p2(input), do: create_password_p2(input, "********", 1)
+  defp create_password_p2(input, password, counter) do
+    {letters, new_counter} = hash(input, counter)
+    {pos, letter} = {String.at(letters,0), String.at(letters,1)}
+    new_pass = update_password_p2(password, Integer.parse(pos), letter)
+    case is_password_found?(new_pass) do
+      true -> new_pass
+      _ -> create_password_p2(input, new_pass, new_counter+1)
+    end
+  end
+
+  defp update_password_p2(password, :error, _letter), do: password
+  defp update_password_p2(password, {pos, _}, letter) do
+    graphemes = String.graphemes(password)
+    case String.at(password, pos) do
+      "*" -> update_password_p2(graphemes, pos, letter)
+      _ -> password
+    end
+  end
+  defp update_password_p2(graphemes, pos, letter) when is_list(graphemes) do
+    Enum.join(
+      Enum.take(graphemes,pos) ++ [letter] ++ Enum.drop(graphemes,pos+1)
+    )
+  end
+
+  def is_password_found?(pass) do
+    IO.inspect(pass)
+    String.graphemes(pass)
+    |> Enum.filter(&(&1=="*"))
+    |> Enum.count() == 0
+  end
 end
--- test/advent_of_code/day5_test.exs
+++ test/advent_of_code/day5_test.exs
@@ -3,15 +3,28 @@ defmodule AdventOfCodeTest.Day5 do
 
   alias AdventOfCode.Day5, as: D5
 
+
   describe "Part 1" do
     # test "md5" do
-    #   assert {"1", 3231929} == D5.hash("abc")
+    #   assert {"155f8105dff7f56ee10fa9b9abd", 3231929} == D5.hash("abc")
     # end
-
+    #
     # @tag timeout: 3_000_000_000
     # test "create_password creates the password" do
     #   assert "f77a0e6e" == D5.create_password("cxdnnyjw")
-    # end    
+    # end
   end
 
+
+  describe "Part 2" do
+    # test "is_password_found determines if there are still * in the password" do
+    #   assert false == D5.is_password_found?("azertyui*")
+    #   assert true == D5.is_password_found?("azertyui")
+    # end
+    #
+    # @tag timeout: 3_000_000_000
+    # test "create_password_p2 creates the password" do
+    #   assert "999828ec" == D5.create_password_p2("cxdnnyjw")
+    # end
+  end
 end
