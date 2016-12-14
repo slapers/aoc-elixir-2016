f348c92ba0c48b0720f129c620e5903ccef91a65
Stefan Lapers <stefan@lapers.be>
2016-12-14 09:16:25 +0100

    day 5

--- lib/advent_of_code/day5.ex
+++ lib/advent_of_code/day5.ex
@@ -1,3 +1,23 @@
 defmodule AdventOfCode.Day5 do
 
+  def hash(input), do: hash(input, 1)
+  def hash(input, counter) do
+    res = :crypto.hash(:md5, "#{input}#{counter}")
+    |> Base.encode16(case: :lower)
+
+    case String.split_at(res, 5) do
+      {"00000", remainder} -> {String.at(remainder, 0), counter}
+      _ -> hash(input, counter + 1)
+    end
+  end
+
+  def create_password(input), do: create_password(input, "", 1)
+  def create_password(input, password, counter) do
+    {letter, new_counter} = hash(input, counter)
+    case String.length(password) do
+      size when size >= 7 -> password <> letter
+      size when size < 7 -> create_password(input, password <> letter, new_counter + 1)
+    end
+  end
+
 end
--- test/advent_of_code/day5_test.exs
+++ test/advent_of_code/day5_test.exs
@@ -1,8 +1,17 @@
 defmodule AdventOfCodeTest.Day5 do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
-  test "" do
-    
+  alias AdventOfCode.Day5, as: D5
+
+  describe "Part 1" do
+    # test "md5" do
+    #   assert {"1", 3231929} == D5.hash("abc")
+    # end
+
+    # @tag timeout: 3_000_000_000
+    # test "create_password creates the password" do
+    #   assert "f77a0e6e" == D5.create_password("cxdnnyjw")
+    # end    
   end
 
 end
